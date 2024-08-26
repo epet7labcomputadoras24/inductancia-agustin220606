4cf2e8f4777308cfd603cbba971a032589926480
agustin220606 <agustindelgado220606@gmail.com>
2024-08-26 15:48:05 -0300

    tarea agustin

---
+++ tarea1.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Tar
+
+Module Program
+    Sub Main(args As String())
+        Dim PI As Single
+        PI = 3.1415
+        Dim L As Single
+        Dim XL As Single
+        Dim F As Single
+        Console.Write("ingrese los valores de L")
+        L = Console.ReadLine()
+        Console.Write("ingrese los valores de F")
+        F = Console.ReadLine
+        XL = PI * 2 * F * L
+        Console.WriteLine("su valor de XL es" & XL)
+    End Sub
+End Module
